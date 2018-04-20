94a76167bccd21fb2fdaa66228a321a3bf5cb5eb
lucieleblanc <lucieleblanc425@gmail.com>
2018-04-20 12:45:15 -0400

    created page with API call and template rendering

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
@@ -7,6 +8,21 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
+@app.route('/pokemon/<int:query>')
+def getNameByID(query):
+	return getPokemonInfo(query, 0)
+	
+@app.route('/pokemon/<string:query>')
+def getIDByName(query):
+	return getPokemonInfo(query, 1)
+ 
+# since the API call is the same,
+# why write the same code twice?
+def getPokemonInfo(requestString, type):
+	pokeInfo = requests.get('http://pokeapi.co/api/v2/pokemon/%s' % requestString)
+	pokeInfo = pokeInfo.json()
+	print (pokeInfo["id"], pokeInfo["name"])
+	return render_template('pokemon.html', id=pokeInfo["id"], name=pokeInfo["name"], type=type)
 
 if __name__ == '__main__':
     app.run()
